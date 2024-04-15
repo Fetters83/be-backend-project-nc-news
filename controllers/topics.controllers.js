cf72b9a58c6e7fdc762309796becab6600502ea4
Fetters83 <wgyves@hotmail.com>
2024-04-15 16:03:02 +0100

    task 2 adding tests and controllers, models and app

---
+++ controllers/topics.controllers.js
@@ -0,0 +1,10 @@
+const {fetchAllTopics} = require('../models/topics.models')
+
+
+function getAllTopics(req,res,next){
+    fetchAllTopics().then((body)=>{
+      res.status(200).send(body.rows)
+    })
+}
+
+module.exports = {getAllTopics}
