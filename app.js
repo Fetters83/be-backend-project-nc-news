b33c13e54e4c4ccf089de9648f140f92d306cb21
Fetters83 <wgyves@hotmail.com>
2024-04-16 16:47:17 +0100

    4-get-article-by-id, add endpoints,test,MC,endpoints.jso

--- app.js
+++ app.js
@@ -1,20 +1,34 @@
 const express = require('express')
 const app = express()
-const {getAllTopics,getAllEndPoints} = require('./controllers/topics.controllers')
+const {getAllTopics} = require('./controllers/topics.controllers')
+const {getArticleById} = require('./controllers/articles.controllers')
 const endpoints = require('./endpoints.json')
-
 app.use(express.json())
 
 app.get('/api/topics',getAllTopics)
+
 
 app.get('/api',(req,res,next)=>{
     res.status(200).send({endpoints})
     
 
 })
+app.get('/api/articles/:article_id',getArticleById)
 
+//Error handling middleware
 
+app.use((err,req,res,next)=>{
+if(err.status && err.msg){
+     res.status(err.status).send({msg:err.msg})
+    }
+    next(err)
+})
 
+app.use((err,req,res,next)=>{
+    if(err.code === "22P02"){
+    res.status(400).send({status:400,msg:"Bad request"})
+    }
+})
 
 
 app.all('*',(req,res,next)=>{
