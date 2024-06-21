3acc2aa1186c4cad2ab0ba6d0b52047020bd5f1b
Fetters83 <wgyves@hotmail.com>
2024-06-21 21:38:38 +0100

    added endpoint for returning user by username and amended endpoints.json

--- models/users.models.js
+++ models/users.models.js
@@ -9,4 +9,13 @@ function fetchAllUsers(){
     })
 }
 
-module.exports = {fetchAllUsers}
+function fetchUserByUserName(username){
+    const queryString = (`SELECT * FROM users WHERE username=$1`)
+    return db.query(queryString,[username]).then(({rows}) =>{
+        if(rows.length === 0) {
+            return Promise.reject({status:404,msg:'user not found'})
+        }
+        return rows[0]
+    })
+}
+module.exports = {fetchAllUsers,fetchUserByUserName}
