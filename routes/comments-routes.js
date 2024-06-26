ac228acc617bfc15a8e39266187daf40eb42dd95
Fetters83 <wgyves@hotmail.com>
2024-06-26 17:19:05 +0100

    added vote funtionality for each comment and updated endpoints.json

--- routes/comments-routes.js
+++ routes/comments-routes.js
@@ -1,8 +1,11 @@
-const {removeCommentById} = require('../controllers/comments.controllers')
+const {removeCommentById, postVoteByCommentId} = require('../controllers/comments.controllers')
 
 const commentsRouter = require('express').Router()
 
 //Delete comment by Id
 commentsRouter.delete('/:comment_id',removeCommentById)
+
+//add vote to comment by comment id
+commentsRouter.patch('/:comment_id',postVoteByCommentId)
 
 module.exports = commentsRouter
